919c3dd29b35cd32fd3dc09a8e9fec7e21df966c
David Armendariz <david.adrian.a.p@gmail.com>
2023-11-25 15:33:22 -0500

    Adds lasso regression

---
+++ python-notebooks/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+def get_significant_variables(result_summary):
+    result_summary_df = result_summary.tables[1]
+    significant_variables = []
+    significance_level = 0.05
+    for i in range(1, len(result_summary_df)):
+        p_value = float(result_summary_df[i][4].data)
+        if p_value < significance_level:
+            significant_variables.append(result_summary_df[i][0].data)
+    print(pd.DataFrame({"Variables significativas": significant_variables}))
