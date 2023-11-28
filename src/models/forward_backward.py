77046a7cdd02d1c43e9d5afd9c7d63945e0a03d8
David Armendariz <david.adrian.a.p@gmail.com>
2023-11-27 21:34:58 -0500

    Fixes forward backward

--- src/models/forward_backward.py
+++ src/models/forward_backward.py
@@ -5,27 +5,29 @@ import pandas as pd
 
 
 def forward_selection(X_train, y_train):
-    sfs = SequentialFeatureSelector(LinearRegression(), direction='forward', scoring='f1', cv=None)
+    sfs = SequentialFeatureSelector(LinearRegression(), direction="forward", cv=None)
     sfs.fit(X_train, y_train)
     selected_features = list(X_train.columns[sfs.get_support()])
-    if 'Date' not in selected_features:
-        selected_features.append('Date')
-    if 'energy_price' not in selected_features:
-        selected_features.append('energy_price')
-    
+    if "Date" not in selected_features:
+        selected_features.append("Date")
+    if "energy_price" not in selected_features:
+        selected_features.append("energy_price")
+
     return selected_features
+
 
 # Backward Selection
 def backward_selection(X_train, y_train):
-    sbs = SequentialFeatureSelector(LinearRegression(), direction='backward', scoring='f1', cv=None)
+    sbs = SequentialFeatureSelector(LinearRegression(), direction="backward", cv=None)
     sbs.fit(X_train, y_train)
     selected_features = list(X_train.columns[sbs.get_support()])
 
-    if 'Date' not in selected_features:
-        selected_features.append('Date')
-    if 'energy_price' not in selected_features:
-        selected_features.append('energy_price')
-    
+    if "Date" not in selected_features:
+        selected_features.append("Date")
+    if "energy_price" not in selected_features:
+        selected_features.append("energy_price")
+
     return selected_features
+
 
 # Aplica forward selection y backward selection a tus datos
